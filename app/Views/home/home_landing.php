ad5a4a06efadbf4ebc0c1e5f084614691dc3dc97
dimfaqot <dimyfaqot@gmail.com>
2025-08-07 23:51:42 +0700

    admin

--- app/Views/home/home_landing.php
+++ app/Views/home/home_landing.php
@@ -1,9 +1,8 @@
 <?= $this->extend('templates/logged') ?>
 
 <?= $this->section('content') ?>
-
-<div class="input-group input-group-sm mb-2">
-    <input type="text" class="form-control bg-dark text-light border-secondary cari_card" placeholder="Cari..." aria-label="Recipient's username" aria-describedby="button-addon2">
-    <button class="btn btn-outline-light form_input" data-order="Add" type="button"><i class="fa-solid fa-circle-plus"></i> <?= menu()['menu']; ?></button>
+<div class="text-center bg-danger p-4 fw-bold" style="font-size: 40px;margin-top:200px">
+    <div>WELCOME</div>
+    -[<span class="bg-dark pb-1"><?= strtoupper(user()['nama']); ?></span>]-
 </div>
 <?= $this->endSection() ?>
