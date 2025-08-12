4613abdab3aecf803ae37e7f42b55294c69bf92c
dimfaqot <dimyfaqot@gmail.com>
2025-08-12 23:56:49 +0700

    print

--- app/Views/guest/nota.php
+++ app/Views/guest/nota.php
@@ -100,7 +100,7 @@
         </table>
 
     </div>
-    <div style="text-align: center;">- Terima Kasih -</div>
+    <div style="text-align: center;margin-top:10px">- Terima Kasih -</div>
 
 </body>
 
