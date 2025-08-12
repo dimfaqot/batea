d90704f3e3a6beff0dafd95c7be00d44ee19f325
dimfaqot <dimyfaqot@gmail.com>
2025-08-12 23:45:46 +0700

    print

--- app/Controllers/Guest.php
+++ app/Controllers/Guest.php
@@ -10,7 +10,7 @@ class Guest extends BaseController
 
         $set = [
             'mode' => 'utf-8',
-            'format' => [50, 160],
+            'format' => [70, 160],
             'orientation' => 'P',
             'margin_left' => 0,
             'margin_right' => 0,
