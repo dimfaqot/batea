9f56d28e29ecd5fa68619130336b79dde36f8fbf
dimfaqot <dimyfaqot@gmail.com>
2025-09-02 23:24:44 +0700

    update transaksi

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -181,7 +181,6 @@ class Transaksi extends BaseController
 
         $data = db(strtolower($jenis))->select('*')
             ->where('lokasi', user()['lokasi'])
-            ->selectSum('biaya')
             ->where("MONTH(FROM_UNIXTIME(tgl))", $bulan)
             ->where("YEAR(FROM_UNIXTIME(tgl))", $tahun)
             ->get()
